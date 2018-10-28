ec511dce790e85f7c3ea2534ddb6d87747da1706
MANANPREET <mananpreetsingh.win@gmail.com>
2018-10-28 17:52:47 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div >
-       Phone Directory
+      <div>
+         <div className="header">
+          Phone Directory
+      </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span>
+          <br></br>
+          <span> Phone </span>
+        </div> 
+        <label htmlfor="name"> Name </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="Manan"/>
       </div>
     );
   }
